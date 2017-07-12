71c138061bdb03bd4a7e6802a4b8ad31c6245611
Ahmed Sherien <eng.a.sherien@gmail.com>
2017-07-12 03:50:33 +0200

    5. Adding Domain Entities

---
+++ AhmedSherien.Domain/Companies/Company.cs
@@ -0,0 +1,13 @@
+﻿using AhmedSherien.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AhmedSherien.Domain.Companies
+{
+    public class Company : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
