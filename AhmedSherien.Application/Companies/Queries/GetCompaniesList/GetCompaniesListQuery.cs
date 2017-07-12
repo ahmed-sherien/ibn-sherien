b6867d8ffa04626eaaa028449cfd89d1afb7f21b
Ahmed Sherien <eng.a.sherien@gmail.com>
2017-07-12 04:07:54 +0200

    7. Adding Application Business in CQRS pattern

---
+++ AhmedSherien.Application/Companies/Queries/GetCompaniesList/GetCompaniesListQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AhmedSherien.Application.Companies.Queries.GetCompaniesList
+{
+    public class GetCompaniesListQuery : IGetCompaniesListQuery
+    {
+        public List<CompanyModel> Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AhmedSherien.Application/Companies/Queries/GetCompaniesList/IGetCompaniesListQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AhmedSherien.Application.Companies.Queries.GetCompaniesList
+{
+    public interface IGetCompaniesListQuery
+    {
+        List<CompanyModel> Execute();
+    }
+}
