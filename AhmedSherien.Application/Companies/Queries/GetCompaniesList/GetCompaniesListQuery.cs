d1e82da42dd5ee7114188e8a985e4c634d99c8e3
Ahmed Sherien <eng.a.sherien@gmail.com>
2017-07-16 21:26:59 +0200

    fix: implementing GetCompaniesListQuery

--- AhmedSherien.Application/Companies/Queries/GetCompaniesList/GetCompaniesListQuery.cs
+++ AhmedSherien.Application/Companies/Queries/GetCompaniesList/GetCompaniesListQuery.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using AhmedSherien.Application.Interfaces.Persistence;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AhmedSherien.Application.Companies.Queries.GetCompaniesList
 {
     public class GetCompaniesListQuery : IGetCompaniesListQuery
     {
+        private readonly ICompaniesRepository _repository;
+        public GetCompaniesListQuery(ICompaniesRepository repository)
+        {
+            _repository = repository;
+        }
         public List<CompanyModel> Execute()
         {
-            throw new NotImplementedException();
+            var companies = _repository.GetAll()
+                .Select(x => new CompanyModel
+                {
+                    Id = x.Id,
+                    Name = x.Name
+                });
+
+            return companies.ToList();
         }
     }
 }
